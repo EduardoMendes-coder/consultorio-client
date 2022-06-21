695bf24061d33a864f5e01f38abb0ab721431b40
Eduardo Mendes <eduardompereira4@outlook.com>
2022-06-21 20:36:34 -0300

    create new project att configs

--- src/client/medico.client.ts
+++ src/client/medico.client.ts
@@ -1,28 +1,28 @@
-import { Paciente } from "@/model/paciente.model";
+import { Medico } from "@/model/medico.model";
 import { PageRequest } from "@/model/page/page-request";
 import { PageResponse } from "@/model/page/page-response";
 import axios, { AxiosInstance } from "axios";
 
-export class PacienteClient {
+export class MedicoClient {
 
     private axiosClient: AxiosInstance;
 
     constructor() {
         this.axiosClient = axios.create({
-            baseURL: 'http://localhost:8080/api/pacientes',
+            baseURL: 'http://localhost:8080/api/medicos',
             headers: {'Content-type' : 'application/json'}
         });
     }
 
-    public async findById(id: number): Promise<Paciente> {
+    public async findById(id: number): Promise<Medico> {
         try {
-            return (await this.axiosClient.get<Paciente>(`/${id}`)).data
-        } catch (error) {
+            return (await this.axiosClient.get<Medico>(`/${id}`)).data
+        } catch (error:any) {
             return Promise.reject(error.response)
         }
     }
 
-    public async findByFiltrosPaginado(pageRequest : PageRequest): Promise<PageResponse<Paciente>> {
+    public async findByFiltrosPaginado(pageRequest : PageRequest): Promise<PageResponse<Medico>> {
         try {
 
             let requestPath = ''
@@ -32,36 +32,36 @@ export class PacienteClient {
             requestPath += `&sort=${pageRequest.sortField === undefined
                 ? '' : pageRequest.sortField},${pageRequest.direction}`
 
-            return (await this.axiosClient.get<PageResponse<Paciente>>(requestPath,
+            return (await this.axiosClient.get<PageResponse<Medico>>(requestPath,
                 {
                     params: { filtros: pageRequest.filter }
                 }
             )).data
-        } catch (error) {
+        } catch (error:any) {
             return Promise.reject(error.response)
         }
     }
 
-    public async cadastrar(paciente: Paciente): Promise<void> {
+    public async cadastrar(medico: Medico): Promise<void> {
         try {
-            return (await this.axiosClient.post('/', paciente))
-        } catch (error) {
+            return (await this.axiosClient.post('/', medico))
+        } catch (error:any) {
             return Promise.reject(error.response)
         }
     }
 
-    public async editar(paciente: Paciente): Promise<void> {
+    public async editar(medico: Medico): Promise<void> {
         try {
-            return (await this.axiosClient.put(`/${paciente.id}`, paciente)).data
-        } catch (error) {
+            return (await this.axiosClient.put(`/${medico.id}`, medico)).data
+        } catch (error:any) {
             return Promise.reject(error.response)
         }
     }
 
-    public async desativar(paciente: Paciente): Promise<void> {
+    public async desativar(medico: Medico): Promise<void> {
         try {
-            return (await this.axiosClient.put(`/desativar/${paciente.id}`, paciente)).data
-        } catch (error) {
+            return (await this.axiosClient.put(`/desativar/${medico.id}`, medico)).data
+        } catch (error:any) {
             return Promise.reject(error.response)
         }
     }
