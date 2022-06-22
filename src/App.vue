ff808ea19d1c10c1b110f2aeed6bfbc9de19f447
Eduardo Mendes <eduardompereira4@outlook.com>
2022-06-21 21:38:45 -0300

    alter port

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 </template>
 
 <style lang="scss">
+@import "~bulma/bulma.sass";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,7 +15,6 @@
   text-align: center;
   color: #2c3e50;
 }
-
 nav {
   padding: 30px;
 
