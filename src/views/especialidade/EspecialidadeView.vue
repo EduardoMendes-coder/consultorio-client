6811306a100702d219d7f0f5767512b355c8df63
Eduardo Mendes <eduardompereira4@outlook.com>
2022-06-22 21:39:31 -0300

    add button cadastrar especialidade

--- src/views/especialidade/EspecialidadeView.vue
+++ src/views/especialidade/EspecialidadeView.vue
@@ -3,6 +3,7 @@
     <h1 class="titulo" >Lista de Especialidades</h1>
     <div class="control">
       <input class="input" type="nome-especialidade" placeholder="Buscar Especialidade">
+      <button class="button btn-cadastrar">Cadastrar</button>
     </div>
   </div>
 </template>
@@ -20,8 +21,19 @@ export default {
   margin-top: 0px;
   margin-outside: 10px;
 }
-.control{
-  margin-top: 20px;
+.input{
   width: 60%;
+}
+.btn-cadastrar{
+  width: 30%;
+  margin-left: 20px;
+  background-color: green;
+  color: white;
+}
+.control{
+  display: flex;
+  margin-top: 20px;
+  width: 100%;
+  flex-direction: row;
 }
 </style>
